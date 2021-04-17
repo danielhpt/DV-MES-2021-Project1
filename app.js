bdec280bb669f00782d64f914b97b24ed19db24f
Paulo Fournier <paulo.fournier@gmail.com>
2021-04-17 17:32:21 +0100

    example

--- app.js
+++ app.js
@@ -39,7 +39,9 @@ const api = {
       res.render('error');
     });
 
+    //exemplo
     let json = await this.getJsonFromCsv('./data/flavors_of_cacao.csv');
+    console.log('json', typeof json);
 
   },
   /**
