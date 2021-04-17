18a768f3b4a96ae56b8a002596043d7174fa7cb2
Paulo Fournier <paulo.fournier@gmail.com>
2021-04-17 17:25:51 +0100

    First project version

--- app.js
+++ app.js
@@ -1,39 +1,52 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors'),
+    express = require('express'),
+    path = require('path'),
+    cookieParser = require('cookie-parser'),
+    logger = require('morgan'),
+    csv = require('csvtojson');
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index'),
+    app = express();
 
-var app = express();
+const api = {
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+  init: function () {
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
 
-app.use('/', indexRouter);
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+    app.use('/', indexRouter);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+      next(createError(404));
+    });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // error handler
+    app.use(function(err, req, res, next) {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+    });
+
+    let json = this.getJsonFromCsv('./data/flavors_of_cacao.csv');
+
+  },
+
+  getJsonFromCsv: async function (csvFilePath) {
+    console.log(await csv().fromFile(csvFilePath))
+  }
+
+}.init();
 
 module.exports = app;
